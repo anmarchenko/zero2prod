e714e8cdcd381c392bba34f07dff270c88b94279
Andrey Marchenko <andrei.marchenko@protonmail.com>
2024-11-17 14:11:50 +0100

    store postgres connection in app_data of the actix app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -9,6 +10,9 @@ async fn main() -> std::io::Result<()> {
     let address = format!("127.0.0.1:{}", configuration.application_port);
 
     let listener = TcpListener::bind(address)?;
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to the database.");
 
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,19 @@
+use crate::routes::health_check;
+use crate::routes::subscribe;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::io::Error;
 use std::net::TcpListener;
 
-use crate::routes::health_check;
-use crate::routes::subscribe;
-
-pub fn run(listener: TcpListener) -> Result<Server, Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, Error> {
+    // wrap connection in a smart pointer (Arc)
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
