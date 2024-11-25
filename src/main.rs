37a3f19a70be0cf950ea1636724cd52b7d288741
Andrey Marchenko <andrei.marchenko@protonmail.com>
2024-11-25 16:32:04 +0100

    logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use env_logger::Env;
+use log::info;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -5,6 +7,9 @@ use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    info!("Starting Zero2Prod");
+
     // Panic if we can't read config
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
