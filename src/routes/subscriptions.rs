2d9a6ff97b867cc5840fe466a922cadbdd922303
Andrey Marchenko <andrei.marchenko@protonmail.com>
2025-03-15 14:44:00 +0100

    use anyhow and more cleanup

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,12 +3,13 @@ use crate::email_client::EmailClient;
 use crate::startup::ApplicationBaseUrl;
 use actix_web::http::StatusCode;
 use actix_web::{web, HttpResponse, ResponseError};
+use anyhow::Context;
 use chrono::Utc;
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 use sqlx::{PgPool, Postgres, Transaction};
 use std::error::Error;
-use std::fmt::{Debug, Display, Formatter};
+use std::fmt::{Debug, Formatter};
 use uuid::Uuid;
 
 #[tracing::instrument(
@@ -27,25 +28,21 @@ pub async fn subscribe(
     let new_subscriber = form.0.try_into().map_err(SubscribeError::ValidationError)?;
 
     // BEGIN TRANSACTION
-    let mut transaction = pool
-        .begin()
-        .await
-        .map_err(|e| SubscribeError::UnexpectedError(Box::new(e), "Pool error".into()))?;
+    let mut transaction = pool.begin().await.context("Pool error")?;
 
     let subscriber_id = insert_subscriber(&mut transaction, &new_subscriber)
         .await
-        .map_err(|e| {
-            SubscribeError::UnexpectedError(Box::new(e), "Insert subscriber error".into())
-        })?;
+        .context("Insert subscriber error")?;
     let subscription_token = generate_subscription_token();
 
     store_token(&mut transaction, subscriber_id, &subscription_token)
         .await
-        .map_err(|e| SubscribeError::UnexpectedError(Box::new(e), "Store token error".into()))?;
+        .context("Store token error")?;
 
-    transaction.commit().await.map_err(|e| {
-        SubscribeError::UnexpectedError(Box::new(e), "transaction commit error".into())
-    })?;
+    transaction
+        .commit()
+        .await
+        .context("transaction commit error")?;
     // COMMIT
 
     send_confirmation_email(
@@ -55,9 +52,7 @@ pub async fn subscribe(
         &subscription_token,
     )
     .await
-    .map_err(|e| {
-        SubscribeError::UnexpectedError(Box::new(e), "send confirmation email error".into())
-    })?;
+    .context("send confirmation email error")?;
 
     Ok(HttpResponse::Ok().finish())
 }
@@ -82,8 +77,8 @@ impl TryFrom<FormData> for NewSubscriber {
 pub enum SubscribeError {
     #[error("{0}")]
     ValidationError(String),
-    #[error("{1}")]
-    UnexpectedError(#[source] Box<dyn Error>, String),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
 }
 
 impl Debug for SubscribeError {
@@ -96,7 +91,7 @@ impl ResponseError for SubscribeError {
     fn status_code(&self) -> StatusCode {
         match self {
             SubscribeError::ValidationError(_) => StatusCode::BAD_REQUEST,
-            SubscribeError::UnexpectedError(_, _) => StatusCode::INTERNAL_SERVER_ERROR,
+            SubscribeError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
@@ -121,11 +116,7 @@ pub async fn insert_subscriber(
         Utc::now()
     )
     .execute(&mut **transaction)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
+    .await?;
 
     Ok(subscriber_id)
 }
@@ -138,7 +129,7 @@ pub async fn store_token(
     transaction: &mut Transaction<'_, Postgres>,
     subscriber_id: Uuid,
     subscription_token: &str,
-) -> Result<(), StoreTokenError> {
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"INSERT INTO subscription_tokens(subscriber_id, subscription_token)
         VALUES ($1, $2)"#,
@@ -146,33 +137,9 @@ pub async fn store_token(
         subscription_token,
     )
     .execute(&mut **transaction)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        StoreTokenError(e)
-    })?;
+    .await?;
 
     Ok(())
-}
-
-pub struct StoreTokenError(sqlx::Error);
-
-impl Display for StoreTokenError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "A database error was encountered while saving token")
-    }
-}
-
-impl Debug for StoreTokenError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        error_chain_fmt(self, f)
-    }
-}
-
-impl Error for StoreTokenError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        Some(&self.0)
-    }
 }
 
 #[tracing::instrument(
