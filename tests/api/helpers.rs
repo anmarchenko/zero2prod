5008afc224b0c19a002dd5c53605e0abd380abd7
Andrey Marchenko <andrei.marchenko@protonmail.com>
2025-04-28 20:16:47 +0200

    redirect to login when user is anuthenticated

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -80,15 +80,16 @@ impl TestApp {
             .unwrap()
     }
 
-    pub async fn get_admin_dashboard(&self) -> String {
+    pub async fn get_admin_dashboard(&self) -> reqwest::Response {
         self.api_client
             .get(format!("{}/admin/dashboard", &self.address))
             .send()
             .await
             .expect("failed to execute request")
-            .text()
-            .await
-            .unwrap()
+    }
+
+    pub async fn get_admin_dashboard_html(&self) -> String {
+        self.get_admin_dashboard().await.text().await.unwrap()
     }
 
     pub fn get_confirmation_links(&self, email_request: &wiremock::Request) -> ConfirmationLinks {
